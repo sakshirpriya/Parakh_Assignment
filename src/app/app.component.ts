6838ebe99c1783c225a825f46760b5421bdaf7f1
Sakshi Priya <sakshipriyakpb@gmail.com>
2020-07-24 11:53:19 +0530

    assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'internship';
+  title = 'Assignment';
+  arr: any = []
+
+  add() {
+    this.arr.push({ num1: '', num2: '', sum: '' })
+  }
+  delete() {
+
+    this.arr.pop()
+  }
+  deleteAtLocation(index){
+    this.arr.splice(index, 1);
+  }
+  givenNum1(value, location) {
+    let num1 = Number(value.target.value)
+    this.arr[location]['num1'] = num1
+  }
+  givenNum2(value, location) {
+    let num2 = Number(value.target.value)
+    this.arr[location]['num2'] = num2
+    this.sumOfTwoNumber(location)
+  }
+  sumOfTwoNumber(location) {
+    console.log(this.arr[location]['num1'] + ' ' + this.arr[location]['num2'])
+    this.arr[location]['sum'] = this.arr[location]['num1'] + this.arr[location]['num2']
+  }
 }
